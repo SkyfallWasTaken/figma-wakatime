142c9c0a2b877838f65ecb55d5bfca254941d642
Mahad Kalam <mahadkalam1@proton.me>
2024-12-15 20:12:07 +0000

    Main world jazz

--- src/entrypoints/content.ts
+++ src/entrypoints/content.ts
@@ -1,6 +1,12 @@
+import pWaitFor from "p-wait-for";
+
 export default defineContentScript({
   matches: ["*://*.figma.com/design/*"],
+  world: "MAIN",
   async main() {
     console.log("Loading WakaTime for Figma...");
+    await pWaitFor(() => window.figma !== undefined, { timeout: 5000 });
+    console.log("I am logging.");
+    console.log("WOAH:" + figma.root.name);
   },
 });
