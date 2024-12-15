a442442fd4d3391fe5f59ad2884a41be24bc34aa
Mahad Kalam <mahadkalam1@proton.me>
2024-12-15 10:59:12 +0000

    Big bang

---
+++ wxt.config.ts
@@ -0,0 +1,14 @@
+import { defineConfig } from "wxt";
+
+// See https://wxt.dev/api/config.html
+export default defineConfig({
+  srcDir: "src",
+  manifest: {
+    name: "WakaTime for Figma",
+    version: "1.0.0",
+    description:
+      "Metrics, insights, and time tracking automatically generated from your Figma activity.",
+  },
+  extensionApi: "chrome",
+  modules: ["@wxt-dev/module-svelte"],
+});
