ded4e1c171e2cfe5061f86a4b89021a35f94400a
Mahad Kalam <mahadkalam1@proton.me>
2024-12-19 13:43:31 +0000

    Remove some unused perms

--- wxt.config.ts
+++ wxt.config.ts
@@ -3,13 +3,13 @@ import { defineConfig } from "wxt";
 // See https://wxt.dev/api/config.html
 export default defineConfig({
   srcDir: "src",
-  modules: ['@wxt-dev/auto-icons'],
+  modules: ["@wxt-dev/auto-icons", "@wxt-dev/module-svelte"],
   manifest: {
     name: "WakaTime for Figma",
     version: "1.0.0",
     description:
       "Metrics, insights, and time tracking automatically generated from your Figma activity.",
-    permissions: ["storage", "scripting", "tabs", "activeTab", "cookies"], // FIXME: reduce perms
+    permissions: ["storage", "activeTab", "cookies"],
     host_permissions: ["<all_urls>"],
     web_accessible_resources: [
       {
@@ -18,6 +18,5 @@ export default defineConfig({
       },
     ],
   },
-  extensionApi: "chrome",
-  modules: ["@wxt-dev/module-svelte"],
+  extensionApi: "chrome"
 });
